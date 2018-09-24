56091bce50fea3d11967e282eb64d0e5c2056489
Iván Perdomo <ivan@perdomo.me>
2018-09-24 11:48:44 +0200

    [#1] Move credentials to environment variables

--- run.sh
+++ run.sh
@@ -14,11 +14,11 @@ cp "${CATALINA_HOME}/webapps/reportserver/WEB-INF/classes/reportserver.propertie
 cp "${CATALINA_HOME}/webapps/reportserver/WEB-INF/classes/logging-rs.properties" /opt/reportserver/logging-rs.properties
 
 sed -i \
-    -e 's|^# hibernate.connection.username=root|hibernate.connection.username=reportserver|' \
-    -e 's|^# hibernate.connection.password=root|hibernate.connection.password=reportserver|' \
-    -e 's|^# hibernate.dialect=net.datenwerke.rs.utils.hibernate.PostgreSQLDialect|hibernate.dialect=net.datenwerke.rs.utils.hibernate.PostgreSQLDialect|' \
-    -e 's|^# hibernate.connection.driver_class=org.postgresql.Driver|hibernate.connection.driver_class=org.postgresql.Driver|' \
-    -e 's|^# hibernate.connection.url=jdbc:postgresql://localhost/reportserver|hibernate.connection.url=jdbc:postgresql://db/reportserver|' \
+    -e "s|^# hibernate.connection.username=root|hibernate.connection.username=${RS_DB_USER}|" \
+    -e "s|^# hibernate.connection.password=root|hibernate.connection.password=${RS_DB_PASSWORD}|" \
+    -e "s|^# hibernate.dialect=net.datenwerke.rs.utils.hibernate.PostgreSQLDialect|hibernate.dialect=net.datenwerke.rs.utils.hibernate.PostgreSQLDialect|" \
+    -e "s|^# hibernate.connection.driver_class=org.postgresql.Driver|hibernate.connection.driver_class=org.postgresql.Driver|" \
+    -e "s|^# hibernate.connection.url=jdbc:postgresql://localhost/reportserver|hibernate.connection.url=jdbc:postgresql://${RS_DB_HOST}/${RS_DB_NAME}|" \
     /opt/reportserver/persistence.properties
 
 echo "Waiting 20s..."
