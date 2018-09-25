46b9659e3d450bc5b43512047089860e18ac5cc1
Iván Perdomo <ivan@perdomo.me>
2018-09-25 17:32:05 +0200

    [#1] Fix shellcheck warnings, use (( expr )) for arithmetic
    Since we're using `set -e` the runtime behavior of the script is
    different. We opt to use the workaround `(( expr )) || : ` as
    described here:
    http://wiki.bash-hackers.org/syntax/arith_expr#arithmetic_expressions_and_return_codes

--- ci/test.sh
+++ ci/test.sh
@@ -2,24 +2,26 @@
 
 set -euo pipefail
 
-URL="http://127.0.01:8080/reportserver/reportserver/httpauthexport?key=installation_test_report&user=testuser&apikey=TEST_API_KEY&format=PDF"
+url="http://127.0.01:8080/reportserver/reportserver/httpauthexport?key=installation_test_report&user=testuser&apikey=TEST_API_KEY&format=PDF"
 
-MAX_ATTEMPTS=120
-ATTEMPTS=0
-RS=""
+max_attempts=120
+attempts=0
+rs=""
 
 echo "Waiting for ReportServer ..."
-while [[ -z "${RS}" && "${ATTEMPTS}" -lt "${MAX_ATTEMPTS}" ]]; do
+while [[ -z "${rs}" && "${attempts}" -lt "${max_attempts}" ]]; do
     sleep 1
-    RS=$( (docker-compose logs --no-color report-server 2>&1 | grep -i "LateInitStartup - Startup completed") || echo "")
-    let ATTEMPTS+=1
+    rs=$( (docker-compose logs --no-color report-server 2>&1 | grep -i "LateInitStartup - Startup completed") || echo "")
+    echo "${attempts}"
+    (( attempts++ )) || :
+    echo "${attempts}"
 done
 
-if [[ -z "${RS}" ]]; then
+if [[ -z "${rs}" ]]; then
     echo "ReportServer is not available"
     exit 1
 fi
 
-docker-compose -f docker-compose.yml -f docker-compose.ci.yml exec report-server curl -o /tmp/test-report.pdf "${URL}"
+docker-compose -f docker-compose.yml -f docker-compose.ci.yml exec report-server curl -o /tmp/test-report.pdf "${url}"
 
 docker-compose -f docker-compose.yml -f docker-compose.ci.yml exec test pdfgrep foobar /tmp/test-report.pdf
