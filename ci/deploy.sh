539cc27317c8e551814a00a12d08b59f3ee3a574
Iván Perdomo <ivan@perdomo.me>
2018-10-01 04:18:55 +0200

    [#1] Update and install required gcloud components
    - Make sure we use gcloud SDK installed in $HOME

--- ci/deploy.sh
+++ ci/deploy.sh
@@ -2,6 +2,12 @@
 
 set -eu -o pipefail
 
+# Install and update gcloud components
+gcloud components install kubectl
+gcloud components update
+gcloud version
+command -v gcloud kubectl
+
 
 docker login -u="${DOCKERHUB_USERNAME}" -p="${DOCKERHUB_PASSWORD}"
 docker push "akvo/akvo-reportserver:${TRAVIS_COMMIT:0:8}"
