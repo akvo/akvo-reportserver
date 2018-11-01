7845b0781a032acbe4519b628e18a4457309273e
stellanl <stellanl@akvo.org>
2018-11-01 17:49:46 +0100

    [#10]Fix unzip switch.

--- run.sh
+++ run.sh
@@ -61,7 +61,7 @@ echo "PostgreSQL is ready!"
 if [ ! -z ${RS_EXTRA_FONT_URL+x} ]; then
     echo "Fetching extra fonts"
     curl --retry 3 --location --output /tmp/extrafonts.zip "${RS_EXTRA_FONT_URL}"
-    unzip -f /tmp/extrafonts.zip -d /usr/share/fonts/truetype/
+    unzip -u /tmp/extrafonts.zip -d /usr/share/fonts/truetype/
     fc-cache -f -v
     rm /tmp/extrafonts.zip
 fi
